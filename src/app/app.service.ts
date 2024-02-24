90c6f011a5c94ea35a131f111d1d33cf4841cb51
Wellington5000 <wellingtonsantos5000@gmail.com>
2024-02-24 16:40:27 -0300

    adicionada integração com a api de acertos

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Concourse } from './models/concourse';
 
 @Injectable({
   providedIn: 'root'
@@ -44,5 +45,19 @@ export class AppService {
     };
 
     return this.http.post(this.API_URL + 'v1/pagamento/checkout/gerar-qr-code', data.body, httpOptions);
+  }
+
+  findAllHits(): Observable<Concourse> {
+    return this.http.get<Concourse>(this.API_URL + 'concursos-acertos-grupos');
+  }
+
+  findHitsByCode(concurso: string): Observable<Concourse> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Accept': 'application/json'
+      })
+    };
+
+    return this.http.post<Concourse>(this.API_URL + 'concursos-acertos-grupos', { concurso: concurso }, httpOptions);
   }
 }
--- src/app/check-hits/check-hits.component.ts
+++ src/app/check-hits/check-hits.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
+import { Hits } from '../models/hits';
+import { NotificationService } from '../utils/notification/notification.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-check-hits',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./check-hits.component.scss']
 })
 export class CheckHitsComponent implements OnInit {
+  hits: Hits[] = [];
+  concourse: FormControl = new FormControl('');
 
-  constructor() { }
+  constructor(
+    private appService: AppService,
+    private notificationService: NotificationService
+  ) { }
 
   ngOnInit(): void {
+    this.getHits();
   }
 
+  getHits(): void {
+    this.appService.findAllHits().subscribe({
+      next: (response) => {
+        this.hits = response.concursos;
+      },
+      error: (error) => {
+        this.notificationService.notify('Erro ao carregar os concursos');
+      }
+    })
+  }
+
+  getHitsByCode(): void {
+    if(this.concourse.value) {
+      this.appService.findHitsByCode(this.concourse.value).subscribe({
+        next: (response) => {
+          this.hits = response.concursos;
+        },
+        error: (error) => {
+          this.notificationService.notify('Concurso não encontrado');
+        }
+      })
+    } else {
+      this.getHits();
+    }
+  }
 }
