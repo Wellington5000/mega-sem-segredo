a365b8d2b7734048741b091fc42d340e4bf46ada
Wellington5000 <wellingtonsantos5000@gmail.com>
2024-12-02 21:15:28 -0300

    main

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -4,12 +4,15 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AppService } from '../app.service';
 import { NotificationService } from '../utils/notification/notification.service';
 
+const REQUEST_PASSWORD_KEY = 'request-password-key';
+
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent implements OnInit {
+  disabledButton: boolean = false;
   hasError: boolean = false;
   isRecovery: boolean = false;
   successChange: boolean = false;
@@ -25,12 +28,19 @@ export class ChangePasswordComponent implements OnInit {
 
   ngOnInit(): void {
     const param = this.activatedRoute.snapshot.params['type'];
+    const requestedPassword = localStorage.getItem(REQUEST_PASSWORD_KEY);
 
     if(!this.params.includes(param)) {
       this.redirect();
     } else {
       this.isRecovery = param === 'recovery';
     }
+
+    if(requestedPassword) {
+      this.disabledButton = true;
+    }
+
+    this.startTimeOut();
   }
 
   redirect(): void {
@@ -38,13 +48,36 @@ export class ChangePasswordComponent implements OnInit {
   }
 
   changePassword(): void {
-    this.appService.recoveryPassword({ email: this.email.value }).subscribe({
+    this.appService.recoveryPassword({ email: this.email.value }).subscribe({ 
       next: (response) => {
         this.successChange = true;
+        this.wasRequestedPassword();
       },
       error: (error) => {
         this.notificationService.notify(error?.error?.message || 'Ocorreu um erro ao recuperar senha. Por favor, tente novamente!');
+
+        if(error?.error?.message === 'Por favor espere antes de tentar novamente.') {
+          this.wasRequestedPassword();
+        } else {
+          this.clearItem();
+        }
       }
     })
+  }
+
+  wasRequestedPassword(): void {
+    localStorage.setItem(REQUEST_PASSWORD_KEY, 'true');
+    this.disabledButton = true;
+  }
+
+  startTimeOut(): void {
+    setTimeout(() => {
+      this.clearItem();
+    }, 600000);
+  }
+
+  clearItem(): void {
+    localStorage.removeItem(REQUEST_PASSWORD_KEY);
+    this.disabledButton = false;
   }
 }
