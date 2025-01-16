ca730d4b95433451206afb7f266014ab9194a490
Wellington5000 <wellingtonsantos5000@gmail.com>
2025-01-15 22:45:47 -0300

    main

--- src/app/lotofacil/lotofacil.component.ts
+++ src/app/lotofacil/lotofacil.component.ts
@@ -12,12 +12,13 @@ import { NotificationService } from '../utils/notification/notification.service'
 export class LotofacilComponent implements OnInit {
   combinationsQuantity: number = 0;
   selectedNumbers: Set<number> = new Set();
+  maxSelectableNumbers: number = 18;
   numbers: string[] = Array.from({ length: 25 }, (_, i) => String(i + 1).padStart(2, '0'));
 
-  private combinationMapping: Record<number, number> = {
-    1: 24,
-    2: 110,
-    3: 356
+  private combinationMapping: Record<number, { quantity: number; maxSelectable: number }> = {
+    1: { quantity: 24, maxSelectable: 18 },
+    2: { quantity: 110, maxSelectable: 19 },
+    3: { quantity: 356, maxSelectable: 20 }
   };
 
   constructor(
@@ -34,20 +35,30 @@ export class LotofacilComponent implements OnInit {
     if (isNaN(combination)) {
       console.warn('Invalid combination parameter:', combinationParam);
       this.combinationsQuantity = 0;
+      this.maxSelectableNumbers = 18;
     } else {
-      this.setCombinationsQuantity(combination);
+      this.setCombinationsProperties(combination);
     }
   }
 
-  setCombinationsQuantity(combination: number): void {
-    this.combinationsQuantity = this.combinationMapping[combination] || 0;
+  setCombinationsProperties(combination: number): void {
+    const mapping = this.combinationMapping[combination];
+    console.log(mapping);
+    console.log(combination)
+    if (mapping) {
+      this.combinationsQuantity = mapping.quantity;
+      this.maxSelectableNumbers = mapping.maxSelectable;
+    } else {
+      this.combinationsQuantity = 0;
+      this.maxSelectableNumbers = 18;
+    }
   }
 
   addOrRemoveNumber(number: number): void {
     if (this.checkNumber(number)) {
-        this.selectedNumbers.delete(number);
+      this.selectedNumbers.delete(number);
     } else {
-      if(this.selectedNumbers.size < 18) {
+      if (this.selectedNumbers.size < this.maxSelectableNumbers) {
         this.selectedNumbers.add(number);
       }
     }
@@ -64,18 +75,18 @@ export class LotofacilComponent implements OnInit {
       dezenas: selectedNumbersArray,
       tipo: this.combinationsQuantity,
       loteria: 'LF'
-    }
+    };
   }
 
   createCombinations(): void {
     this.lotteryService.createCombinations(this.getBodyLotofacil()).subscribe({
       next: (response) => {
         this.notificationService.success('Combinações geradas com sucesso!');
-        this.router.navigateByUrl('/lottery/lotofacil')
+        this.router.navigateByUrl('/lottery/lotofacil');
       },
       error: (error) => {
         this.notificationService.notify(error?.error?.erro || 'Ocorreu um erro ao gerar as combinações!');
       }
-    })
+    });
   }
 }
